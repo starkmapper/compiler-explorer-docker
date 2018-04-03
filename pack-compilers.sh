8e99b04227bf8c4bd29a7b0e2f2614141150c122
Mark Stapper <mark.stapper@rhmarinegroup.com>
2018-04-03 11:29:34 +0200

    Add newfangled compiler definition files, gitignore and creation script for those files

---
+++ pack-compilers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+original_pwd=`pwd`
+cd /c
+base_dir=$(printf %q "Program Files (x86)/Embarcadero/Studio/")
+versions=`eval ls $base_dir`
+echo $versions
+# exclude full by default
+#archives="full bin msbuild include lib"
+archives="bin include lib"
+#archives="msbuild"
+for version in $versions
+do
+
+  echo "Packing files for release $version"
+
+  full_dir="$base_dir$version"
+  #just grab the whole dir
+  full_files="$full_dir"
+
+  # specifying individual files might save 10mB, save that for later
+  # we need the executables (bcc32, ilink, brc32 etc), and their config files
+  bin_files="$full_dir/bin/*.exe $full_dir/bin/*.cfg $full_dir/bin/rsvars.bat"
+  # We need the built task assemblies and msbuild target files.
+  msbuild_files="$full_dir/bin/*.Targets $full_dir/bin/Borland.Build.* $full_dir/bin/bccide.dll $full_dir/bin/comp32x.dll $full_dir/bin/Borland.Globalization.dll"
+  include_files="$full_dir/include"
+  lib_files="$full_dir/lib"
+
+  for archive in $archives
+  do
+    files_var="$archive"_files
+    eval archive_files='$'$files_var
+    echo Creating archive: $archive
+    eval tar czf '"$original_pwd/compilers/$version-$archive.tar.gz"' $archive_files
+  done
+done
+cd "$original_pwd"
